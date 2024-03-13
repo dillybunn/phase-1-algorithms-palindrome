788b486f1362835a00ad189ac767b1c9f08e1c15
Billy Dunn <bdunn27@gmail.com>
2024-03-13 17:48:53 -0400

    palindrome

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
 */
-
+// // const isPalindrom = (word) => {
+// //   const reversedWord = word.split("").reverse().join("");
+// //   return word === reversedWord;
+// };
 /*
   Add written explanation of your solution here
-*/
+// I had to look up the terminology or syntax for a way to flip a word around. By breaking the string of word into seperate individual letter strings you can reverse the order of the string 
+and then rejoin them. I realize this might cause issues with capital letters so thinking about that, I could have done something like const lowerCaseWord = word.toLowerCase() */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
